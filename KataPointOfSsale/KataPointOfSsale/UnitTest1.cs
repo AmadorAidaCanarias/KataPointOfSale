7253be4fa07de5bd42d18a1a3fa3003e3dea8a91
AmadorAidaCanarias <amlopez@aidacanarias.com>
2022-06-30 09:16:37 +0100

    First Commit

---
+++ KataPointOfSsale/KataPointOfSsale/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace KataPointOfSsale {
+    public class Tests {
+
+        private ScanCode scanCode;
+        
+        [SetUp]
+        public void Setup() {
+            scanCode = new ScanCode();
+        }
+    }
+
+    public class ScanCode {
+
+    }
+}
